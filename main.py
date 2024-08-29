ddb97c1a43c55ce6422a955f511a8a42862ff20d
bennyyang11 <bennyyang11@gmail.com>
2024-08-29 15:39:10 -0600

    checking valid moves

--- main.py
+++ main.py
@@ -274,3 +274,11 @@ def check_knight(position, color):
         if target not in friends_list and 0 <= target[0] <= 7 and 0 <= target[1] <= 7:
             moves_list.append(target)
     return moves_list
+# check for valid moves for just selected piece
+def check_valid_moves():
+    if turn_step < 2:
+        options_list = white_options
+    else:
+        options_list = black_options
+    valid_options = options_list[selection]
+    return valid_options
